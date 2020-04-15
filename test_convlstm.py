840a2bc22c30c5b46be21a37577d36530cb5a310
Milan Desai <milanjdesai@gmail.com>
2020-04-15 01:15:20 +0000

    ConvLSTM test - reduce batch size

--- test_convlstm.py
+++ test_convlstm.py
@@ -1,10 +1,22 @@
 import torch
 import convlstm
 
-torch.nn.LSTM()
+SEQ_LEN = 16
+BATCH_SIZE = 8
+IN_CHANS = 256
+HIDDEN_CHANS = 150
+HEIGHT = 32
+WIDTH = 32
+KERNEL_SIZE=5
+LAYERS = 3
+DROPOUT = 0.5
+BIDIRECTIONAL=True
 
-data = torch.arange(16*128*256*32*32).reshape(16, 128, 256, 32, 32).float().cuda()
-lstm = convlstm.ConvLSTM(256, 150, 5, 3, bidirectional=True, dropout=0.5).cuda()
+size = SEQ_LEN * BATCH_SIZE * IN_CHANS * HEIGHT * WIDTH
+data = torch.arange(size).reshape(SEQ_LEN, BATCH_SIZE, IN_CHANS, HEIGHT, WIDTH).float().cuda()
+lstm = convlstm.ConvLSTM(IN_CHANS, HIDDEN_CHANS, KERNEL_SIZE, LAYERS, bidirectional=BIDIRECTIONAL, dropout=DROPOUT).cuda()
 out = lstm(data)
-assert(tuple(out.size()) == (16, 128, 512, 32, 32))
+
+expected_chans = HIDDEN_CHANS * 2 if BIDIRECTIONAL else HIDDEN_CHANS
+assert(tuple(out.size()) == (SEQ_LEN, BATCH_SIZE, expected_chans, HEIGHT, WIDTH))
 print(out.mean())
